e1f62683372cec4d9680b6fadd2dc966797ea991
Lynda Faller <2335870F@student.gla.ac.uk>
2019-08-07 17:36:54 +0100

    got entry page to be in three columns

--- entries/models.py
+++ entries/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.template.defaultfilters import slugify
 from django.contrib.auth.models import User
 from datetime import *
+from django.utils import timezone
+
 from django.utils import *
 # Create your models here.
 
